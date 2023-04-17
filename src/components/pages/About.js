5f3bb6752eb704b27cea6e72f9af55128bb7dee1
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-17 10:47:01 -0600

    added helpers

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,7 +17,7 @@ export default function About() {
         University of Utah's Professional Education Bootcamp for Full Stack Web
         Development. I have been spending the last 2 years fine tuning my skills
         in JavaScript, Node.js, Express.js, and React.js. My forte is backend.
-        Im skilled in the usage of MySQL, Apollo, Insomnia, and TablePlus. I
+        Im skilled in the usage of MySQL, MongoDB, Apollo, Insomnia, and TablePlus. I
         currently live in Mccammon, Idaho with my wife and 2 children. In 5 years I
         hope to be established in a backend engineering career, working my days
         away at home.
@@ -25,8 +25,8 @@ export default function About() {
       
       <div>
         <img class="w-25" src={BeFunkyPhoto2} alt="me"></img>
-        <img class="w-25 p-3" src={javascript1} alt="3"></img>
-        <img class="w-25 p-3" src={mysql} alt="sql"></img>
+        <img class="w-25 p-3" src={javascript1} alt="1"></img>
+        <img class="w-25 p-3" src={mysql} alt="SQL"></img>
         <img class="w-25 p-3" src={logo512} alt="react"></img>
         <img class="w-25 p-3" src={node} alt="node"></img>
         <img class="w-25 p-3" src={apolloClient} alt="apollo"></img>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,14 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1>Leo Pignanelli's Portfolio</h1>
-      <img alt="" src="files/Animation/new.gif" height="16" width="32"></img>
       <h3>
         What's better than owning your own home? Owning your own Home Page!! Who
         needs a house when you can just program out in the street. Maybe a
         passing Oracle will toss me a job.
       </h3>
 
-        <a href="https://github.com/hashketchum208/REST_2_MERN">REST_2_MERN</a>
+        <a href="https://github.com/hashketchum208">GitHub stuff</a>
         
     </div>
   );
